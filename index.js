f8c1d4888b5156379793019689c789ff170026b2
Melike KILIC <melike180400@gmail.com>
2019-09-15 20:21:06 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// add solution here
+function theBeatlesPlay(musicians,instruments){
+  var allTheMusicians= [];
+  for(var i=0;i<musicians.length;i++)
+  allTheMusicians.push(musicians[i] + " plays " + instruments[i]);
+
+    return allTheMusicians;
+}
+function johnLennonFacts(facts){
+   var result=[];
+   var i = 0;
+    while (i < facts.length) {
+        result.push(facts[i] +"!!!");
+        i++;
+    }
+    return result;
+  }
+function iLoveTheBeatles(number){
+var empty=[];
+  do{
+  empty.push("I love the Beatles!")
+  number++;
+}while(number<15);
+return empty;
+}
