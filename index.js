bb2d3994561872e1d5a46c6d63270a68160eb64b
Melike KILIC <melike180400@gmail.com>
2019-09-15 20:13:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,23 +5,23 @@ function theBeatlesPlay(musicians,instruments){
 
     return allTheMusicians;
 }
-// function johnLennonFacts(){
-//    var facts= [ "He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
-//  var hisFacts = [];
-//     var i = 0;
-//     while (i < facts.length) {
-//         hisFacts.push(facts[i] +"!!!");
-//         i++;
-//     }
-//     return hisFacts;
-//   }
-function johnLennonFacts() {
-    var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
-    var newFacts = [];
+function johnLennonFacts(){
+   var facts= [ "He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
+ var hisFacts = [];
     var i = 0;
     while (i < facts.length) {
-        newFacts.push(facts[i] +"!!!");
+        hisFacts.push(facts[i] +"!!!");
         i++;
     }
-    return newFacts;
-}
+    return hisFacts;
+  }
+// function johnLennonFacts() {
+//     var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
+//     var newFacts = [];
+//     var i = 0;
+//     while (i < facts.length) {
+//         newFacts.push(facts[i] +"!!!");
+//         i++;
+//     }
+//     return newFacts;
+// }
