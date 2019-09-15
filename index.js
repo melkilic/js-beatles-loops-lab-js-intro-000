8e6530897a45aa61196ce507ef9aeaa02f790792
Melike KILIC <melike180400@gmail.com>
2019-09-15 19:45:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// add solution here
+function theBeatlesPlay(musicians,instruments){
+  var allTheMusicians= [];
+  for(var i=0;i<musicians.length;i++);
+  allTheMusicians.push(musicians[i] + " plays " + instruments[i]);
+
+    return allTheMusicians;
+}// add solution here
