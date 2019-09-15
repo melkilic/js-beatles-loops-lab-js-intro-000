0726d9d97b07aab0e36ed592667c618ac9d09770
Melike KILIC <melike180400@gmail.com>
2019-09-15 19:51:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-// function theBeatlesPlay(musicians,instruments){
-//   var allTheMusicians= [];
-//   for(var i=0;i<musicians.length;i++);
-//   allTheMusicians.push(musicians[i] + " plays " + instruments[i]);
-//
-//     return allTheMusicians;
-// }// add solution here
-function theBeatlesPlay(players, instruments) {
-    var allPlayers = [];
-    for (var i=0; i < players.length; i++) {
-        allPlayers.push(players[i] + " plays " + instruments[i]);
-    }
-    return allPlayers;
-}
+function theBeatlesPlay(musicians,instruments){
+  var allTheMusicians= [];
+  for(var i=0;i<musicians.length;i++)
+  allTheMusicians.push(musicians[i] + " plays " + instruments[i]);
+
+    return allTheMusicians;
+}// add solution here
+// function theBeatlesPlay(players, instruments) {
+//     var allPlayers = [];
+//     for (var i=0; i < players.length; i++) {
+//         allPlayers.push(players[i] + " plays " + instruments[i]);
+//     }
+//     return allPlayers;
+// }
