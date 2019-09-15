b3e128e20552a5e60448384d29a10a72f8a1f8c5
Melike KILIC <melike180400@gmail.com>
2019-09-15 20:12:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,13 +5,23 @@ function theBeatlesPlay(musicians,instruments){
 
     return allTheMusicians;
 }
-function johnLennonFacts(){
-   var facts= [ "He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
- var hisFacts = [];
+// function johnLennonFacts(){
+//    var facts= [ "He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
+//  var hisFacts = [];
+//     var i = 0;
+//     while (i < facts.length) {
+//         hisFacts.push(facts[i] +"!!!");
+//         i++;
+//     }
+//     return hisFacts;
+//   }
+function johnLennonFacts() {
+    var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
+    var newFacts = [];
     var i = 0;
     while (i < facts.length) {
-        hisFacts.push(facts[i] +"!!!");
+        newFacts.push(facts[i] +"!!!");
         i++;
     }
-    return hisFacts;
-  }
+    return newFacts;
+}
