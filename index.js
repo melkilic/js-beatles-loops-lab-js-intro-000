9390615c7032eb38daf04ad875d9a7ac508a0734
Melike KILIC <melike180400@gmail.com>
2019-09-15 20:15:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function theBeatlesPlay(musicians,instruments){
 
     return allTheMusicians;
 }
-function johnLennonFacts(){
+function johnLennonFacts(facts){
    var facts= [ "He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
  var hisFacts = [];
     var i = 0;
