57dc4dc90114a804b8b6b8480211cc419f6fcfcb
Melike KILIC <melike180400@gmail.com>
2019-09-15 19:47:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 //   var allTheMusicians= [];
 //   for(var i=0;i<musicians.length;i++);
 //   allTheMusicians.push(musicians[i] + " plays " + instruments[i]);
-// 
+//
 //     return allTheMusicians;
 // }// add solution here
 function theBeatlesPlay(players, instruments) {
